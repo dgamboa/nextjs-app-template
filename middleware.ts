718fbe8749226d3e52ad4e727040e2d9ec728c90
Daniel Gamboa <danielgamboa10@gmail.com>
2025-02-14 15:50:19 -0600

    Third commit: template

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// Add protected routes here
+const isProtectedRoute = createRouteMatcher(["/placeholder"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  // If the user isn't signed in and the route is private, redirect to sign-in
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn({ returnBackUrl: "/login" });
+  }
+
+  // If the user is logged in and the route is protected, let them view.
+  if (userId && isProtectedRoute(req)) {
+    return NextResponse.next();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+};
